4f3fea3df12cf0be2bb56e587fdd2ac6befa3f16
Matteo Trentin <matteo.trentin2@unibo.it>
2023-11-08 15:56:10 +0100

    add vpn firewall rule, move vm zone

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -22,31 +22,11 @@ variable "belgium_names" {
   }
 }
 
-# variable "oregon_names" {
-#   type = map(string)
-
-#   default = {
-#     # controller2 = "e2-medium"
-#     worker2 = "n1-standard-1"
-#     worker4 = "n1-standard-1"
-#     worker6 = "n1-standard-1"
-#   }
-# }
-
-# variable "toronto_names" {
-#   type = map(string)
-
-#   default = {
-#     controller3 = "e2-medium"
-#     worker4 = "n1-standard-1"
-#   }
-# }
-
 provider "google" {
   credentials = file("credentials.json")
   project     = var.project
-  region      = "europe-west1"
-  zone        = "europe-west1-b"
+  region      = "europe-west3"
+  zone        = "europe-west3-b"
 }
 
 resource "google_compute_network" "ow_network" {
@@ -60,7 +40,6 @@ resource "google_compute_firewall" "ssh-rule" {
     protocol = "tcp"
     ports    = ["22"]
   }
-  #   target_tags = ["k8s-control-plane", "controller1", "controller2", "controller3", "worker1", "worker2", "worker3", "worker4"]
   source_ranges = ["0.0.0.0/0"]
 }
 
@@ -74,6 +53,16 @@ resource "google_compute_firewall" "fl-invoke-rule" {
   source_ranges = ["${var.allowed_ip}"]
 }
 
+resource "google_compute_firewall" "openvpn-rule" {
+  name    = "openvpn-server-allow"
+  network = google_compute_network.ow_network.name
+  allow {
+    protocol = "udp"
+    ports    = ["1194"]
+  }
+  source_ranges = ["${var.allowed_ip}"]
+}
+
 resource "google_compute_firewall" "private-ports" {
   name    = "private-all-enabled"
   network = google_compute_network.ow_network.name
@@ -82,8 +71,6 @@ resource "google_compute_firewall" "private-ports" {
     # ports    = ["6443"]
   }
   source_tags = ["private"]
-  # source_tags = ["k8s-control-plane", "controller1", "controller2", "controller3", "worker1", "worker2", "worker3", "worker4"]
-  # source_ranges = [""]
 }
 
 ########### Belgium VMs (with k8s control_plane)
@@ -107,7 +94,7 @@ resource "google_compute_instance" "control_plane" {
 resource "google_compute_instance" "europe_vms" {
   for_each     = var.belgium_names
   name         = each.key
-  zone         = "europe-west1-b"
+  zone         = "europe-west3-b"
   machine_type = each.value
   boot_disk {
     initialize_params {
@@ -123,68 +110,17 @@ resource "google_compute_instance" "europe_vms" {
   tags     = ["private"]
 }
 
-
-# ############ Oregon VMs
-# resource "google_compute_instance" "us_east_vms" {
-#   for_each     = var.oregon_names
-#   name         = each.key
-#   zone         = "us-east1-c"
-#   machine_type = each.value
-#   boot_disk {
-#     initialize_params {
-#       size  = 20
-#       image = "ubuntu-os-cloud/ubuntu-2004-lts"
-#     }
-#   }
-#   network_interface {
-#     network = google_compute_network.ow_network.name
-#     access_config {}
-#   }
-#   metadata = { ssh-keys = "${var.gc_user}:${file("../ow-gcp-key.pub")}" }
-#   tags     = ["private"]
-# }
-
-# ############ Toronto VMs
-# resource "google_compute_instance" "toronto_vms" {
-#   for_each     = var.toronto_names
-#   name         = each.key
-#   zone         = "northamerica-northeast2-a"
-#   machine_type = each.value
-#   boot_disk {
-#     initialize_params {
-#       size = 20
-#       image = "ubuntu-os-cloud/ubuntu-2004-lts"
-#     }
-#   }
-#   network_interface {
-#     network = google_compute_network.ow_network.name
-#     access_config {}
-#   }
-#   metadata = { ssh-keys = "${var.gc_user}:${file("../ow-gcp-key.pub")}" }
-#   tags = ["private"]
-# }
-
 resource "local_file" "hosts" {
   content = templatefile("hosts.tmpl",
     {
       control_ip = google_compute_instance.control_plane.network_interface.0.access_config.0.nat_ip
       worker1_ip = google_compute_instance.europe_vms["controller1"].network_interface.0.access_config.0.nat_ip
       worker2_ip = google_compute_instance.europe_vms["worker1"].network_interface.0.access_config.0.nat_ip
-      # worker3_ip    = google_compute_instance.us_east_vms["worker2"].network_interface.0.access_config.0.nat_ip
       worker4_ip = google_compute_instance.europe_vms["worker3"].network_interface.0.access_config.0.nat_ip
-      # worker5_ip    = google_compute_instance.us_east_vms["worker4"].network_interface.0.access_config.0.nat_ip
       worker6_ip = google_compute_instance.europe_vms["worker5"].network_interface.0.access_config.0.nat_ip
-      # worker7_ip    = google_compute_instance.us_east_vms["worker6"].network_interface.0.access_config.0.nat_ip
       private_w2_ip = google_compute_instance.europe_vms["worker1"].network_interface.0.network_ip
-      # private_w3_ip = google_compute_instance.us_east_vms["worker2"].network_interface.0.network_ip
       private_w4_ip = google_compute_instance.europe_vms["worker3"].network_interface.0.network_ip
-      # private_w5_ip = google_compute_instance.us_east_vms["worker4"].network_interface.0.network_ip
       private_w6_ip = google_compute_instance.europe_vms["worker5"].network_interface.0.network_ip
-      # private_w7_ip = google_compute_instance.us_east_vms["worker6"].network_interface.0.network_ip
-      # worker4_ip = google_compute_instance.us_east_vms["controller2"].network_interface.0.access_config.0.nat_ip
-      # worker4_ip = google_compute_instance.us_east_vms["worker3"].network_interface.0.access_config.0.nat_ip
-      # worker6_ip = google_compute_instance.toronto_vms["controller3"].network_interface.0.access_config.0.nat_ip
-      # worker7_ip = google_compute_instance.toronto_vms["worker4"].network_interface.0.access_config.0.nat_ip
       user = var.gc_user
     }
   )
