06a9f255ce08b0bfb4a54fec855412b3535ddca3
Giuseppe De Palma <dpl.gsp@tutanota.com>
2023-11-23 14:01:25 +0100

    feat: add knative deployment

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -48,7 +48,7 @@ resource "google_compute_firewall" "fl-invoke-rule" {
   network = google_compute_network.ow_network.name
   allow {
     protocol = "tcp"
-    ports    = ["30210"]
+    ports    = ["30210", "31080"]
   }
   source_ranges = ["${var.allowed_ip}"]
 }
@@ -113,16 +113,16 @@ resource "google_compute_instance" "europe_vms" {
 resource "local_file" "hosts" {
   content = templatefile("hosts.tmpl",
     {
-      control_ip = google_compute_instance.control_plane.network_interface.0.access_config.0.nat_ip
+      control_ip         = google_compute_instance.control_plane.network_interface.0.access_config.0.nat_ip
       private_control_ip = google_compute_instance.control_plane.network_interface.0.network_ip
-      worker1_ip = google_compute_instance.europe_vms["controller1"].network_interface.0.access_config.0.nat_ip
-      worker2_ip = google_compute_instance.europe_vms["worker1"].network_interface.0.access_config.0.nat_ip
-      worker4_ip = google_compute_instance.europe_vms["worker3"].network_interface.0.access_config.0.nat_ip
-      worker6_ip = google_compute_instance.europe_vms["worker5"].network_interface.0.access_config.0.nat_ip
-      private_w2_ip = google_compute_instance.europe_vms["worker1"].network_interface.0.network_ip
-      private_w4_ip = google_compute_instance.europe_vms["worker3"].network_interface.0.network_ip
-      private_w6_ip = google_compute_instance.europe_vms["worker5"].network_interface.0.network_ip
-      user = var.gc_user
+      worker1_ip         = google_compute_instance.europe_vms["controller1"].network_interface.0.access_config.0.nat_ip
+      worker2_ip         = google_compute_instance.europe_vms["worker1"].network_interface.0.access_config.0.nat_ip
+      worker4_ip         = google_compute_instance.europe_vms["worker3"].network_interface.0.access_config.0.nat_ip
+      worker6_ip         = google_compute_instance.europe_vms["worker5"].network_interface.0.access_config.0.nat_ip
+      private_w2_ip      = google_compute_instance.europe_vms["worker1"].network_interface.0.network_ip
+      private_w4_ip      = google_compute_instance.europe_vms["worker3"].network_interface.0.network_ip
+      private_w6_ip      = google_compute_instance.europe_vms["worker5"].network_interface.0.network_ip
+      user               = var.gc_user
     }
   )
   filename = "../ansible/hosts.ini"
